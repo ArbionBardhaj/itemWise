dbbec7ec912401c996a8f2f239a00df6da065e16
ArbionBardhaj <bardhajarbion@gmail.com>
2023-11-11 21:43:25 +0100

    second commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ const cors = require("cors")
 
 const app = express()
 
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+app.get("/", (req,res)=>{
+    res.send("home")
+})
+
 const PORT = process.env.PORT || 5000
 
 mongoose
