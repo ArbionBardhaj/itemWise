cd4b451d88889a2e108701dd3d3db9791d6ca7be
ArbionBardhaj <bardhajarbion@gmail.com>
2023-12-08 23:57:50 +0100

    commit 7

--- frontend/src/pages/auth/Reset.js
+++ frontend/src/pages/auth/Reset.js
@@ -1,34 +1,40 @@
-import React from 'react'
-import styles from "./auth.module.scss"
-import "./forgotPassword.scss"
-import Card from '../../components/card/Card'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./auth.module.scss";
+import "./forgotPassword.scss";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
 function Reset() {
   return (
     <div>
-    <div className={`container ${styles.auth}`}>
-   <Card>
-       <div className={styles.form}>
-    
-        <h2>Forgot Password</h2>
-        <form>
-            <input type="password" required placeholder='New Password' />
-            <input type="password" required placeholder='Confirm New Password' />
+      <div className={`container ${styles.auth}`}>
+        <Card>
+          <div className={styles.form}>
+            <h2>Forgot Password</h2>text
+            <form>
+              <input type="password" required placeholder="New Password" />
+              <input
+                type="password"
+                required
+                placeholder="Confirm New Password"
+              />
 
-            <button type='submit' className='btn'>Reset</button>
-
-        </form>
+              <button type="submit" className="btn">
+                Reset
+              </button>
+            </form>
+          </div>
+          <div className={styles.links}>
+            <Link to="/" className="link1">
+              - Home
+            </Link>
+            <Link to="/Login" className="link2">
+              - Login
+            </Link>
+          </div>
+        </Card>
+      </div>
     </div>
-    <div className={styles.links}>
-     <Link to="/" className='link1'>- Home</Link>
-     <Link to="/Login"className='link2'>- Login</Link>
-    </div>
-
-   </Card>
-</div>
- 
-</div>
-  )
+  );
 }
 
-export default Reset
+export default Reset;
