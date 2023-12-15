3970a6b6756e7b53dafc05c85a25b3e5e63189b9
ArbionBardhaj <bardhajarbion@gmail.com>
2023-12-15 19:05:32 +0100

    auth update

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,96 +1,115 @@
-import React from 'react'
-import styles from "./auth.module.scss"
-import Card from '../../components/card/Card'
-import { Link, useNavigate } from 'react-router-dom'
-import { useState } from 'react'
-import { toast } from 'react-toastify'
-import { useDispatch } from 'react-redux'
-import {  loginUser, validateEmail } from "../../services/authService";
-import {SET_LOGIN, SET_NAME} from "../../redux/features/auth/authSlice"
-
+import React from "react";
+import styles from "./auth.module.scss";
+import Card from "../../components/card/Card";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { loginUser, validateEmail } from "../../services/authService";
+import { SET_LOGIN, SET_NAME } from "../../redux/features/auth/authSlice";
 
 const initialState = {
- 
   email: "",
   password: "",
-  name:""
-
+  name: "",
 };
 
 function Login() {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState(initialState);
-  const { email, password} = formData;
   const [isLoading, setIsLoading] = useState(false);
-
-
+  const { email, password } = formData;
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const login = async(e)=>{
-      e.preventDefault()
+  const handlePasswordChange = (e) => {
+    const { value } = e.target;
+    if (value.length >= 8) {
+      setFormData({ ...formData, password: value });
+    }
+  };
+  const login = async (e) => {
+    e.preventDefault();
 
-      if( !email || !password ){
-        return toast.error("All fields are required")
-  
-      }
-    
-      if(password.length < 8 ){
-        return toast.error("You need to enter a minimum of 8 characters")
-  
-      }
-      if(!validateEmail(email)){
-        return toast.error("Please enter a valid email")
+    try {
+      if (!email || !password) {
+        throw new Error("All fields are required");
       }
 
-      const userData = {
-         email, password
+      if (password.length < 8) {
+        throw new Error("You need to enter a minimum of 8 characters");
       }
-      setIsLoading(true)
-      try {
-        const data = await loginUser(userData)
-        console.log(data)
-        await dispatch(SET_LOGIN(true))
-        await dispatch(SET_NAME(data.name))
-        navigate("/dashboard")
-        setIsLoading(false)
-      } catch (error) {
-        setIsLoading(false)
-        console.log(error.message);
+
+      if (!validateEmail(email)) {
+        throw new Error("Please enter a valid email");
       }
-  
-  }
 
+      const userData = { email, password };
+      setIsLoading(true);
 
-  
+      const data = await loginUser(userData);
+      console.log(data);
+
+      dispatch({ type: SET_LOGIN(), payload: true });
+      dispatch({ type: SET_NAME(), payload: data.name });
+      
+
+      navigate("/dashboard");
+    } catch (error) {
+      console.error(error.message);
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className={`container ${styles.auth}`}>
-        <Card>
-            <div className={styles.form}>
-         
-             <h2>Login</h2>
-             <form onSubmit={login}>
-                 <input type="email" required placeholder='email' value={email} onChange={handleInputChange} />
-                 <input type="password" required placeholder='password' onChange={handleInputChange} value={password} />
-                 <button type='submit' className='--btn'>Login</button>
+      <Card>
+        <div className={styles.form}>
+          <h2>Login</h2>
+          <form onSubmit={login}>
+          <input
+              type="email"
+              required
+              placeholder="email"
+              name="email"
+              value={email}
+              onChange={handleInputChange} 
+            />
+            <input
+              type="password"
+              required
+              placeholder="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <button type="submit" className="--btn" disabled={isLoading}>
+              {isLoading ? "Logging in..." : "Login"}
+            </button>
+          </form>
+          <Link to="/forgot" className="link">
+            Forgot Password
+          </Link>
 
-             </form>
-             <Link to="/forgot" className='link'>Forgot Password</Link>
-
-             <span className={styles.register}>
-              <Link to="/" className='link'>Home</Link>
-              <p>&nbsp; Don't have an account? &nbsp;</p>
-              <Link to="/register" className='link'>Register</Link>
-             </span>
-            </div>
-        </Card>
+          <span className={styles.register}>
+            <Link to="/" className="link">
+              Home
+            </Link>
+            <p>&nbsp; Don't have an account? &nbsp;</p>
+            <Link to="/register" className="link">
+              Register
+            </Link>
+          </span>
+        </div>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
